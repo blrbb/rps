4b810ba08d455c4d0c8262ffe6e709fc787bf405
brobb <17446452+blrbb@users.noreply.github.com>
2023-07-14 16:47:54 -0700

    Create Rock Paper Scissors game

---
+++ rps-game.js
@@ -0,0 +1,82 @@
+// Randomly returns rock,paper,scissors
+function getComputerChoice() {
+    let randomSelection = Math.ceil(Math.random() * 3);
+    let result;
+
+    switch (randomSelection) {
+        case 1:
+            return "rock"
+
+        case 2:
+            return "paper";
+
+        case 3:
+            return "scissors";
+    }
+}
+
+function playRound(playerSelection = "", computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        alert("Oh dang, it's a tie! you both played " + playerSelection);
+        return "tie";
+    }
+
+    switch (playerSelection + " " + computerSelection) {
+        case "rock scissors":
+        case "scissors paper":
+        case "paper rock":
+            alert("Congratulatations! You win!!!! " + capitalize(playerSelection) + " beats " + capitalize(computerSelection));
+            return "win";
+
+        default:
+            alert("You lose :( " + capitalize(computerSelection) + " beats " + capitalize(playerSelection));
+            return "lose";
+
+    }
+
+    function capitalize(input) {
+        // debugger;
+        let result = input.replace(input, input.toLowerCase()).replace(input.charAt(0), input.charAt(0).toUpperCase());
+        return result;
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let cpuScore = 0;
+    let roundNum = 1;
+
+    while (playerScore + cpuScore < 5) {
+        let playerSelection;
+        playerSelection = prompt("Round: " + roundNum + "\n\nRock, Paper or Scissors?");
+
+        playerSelection = playerSelection.toLowerCase();
+        let cpuSelection = getComputerChoice();
+
+        while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+            playerSelection = prompt(playerSelection + " is not a valid option in Rock Paper Scissors, please try again.");
+        }
+        let result = playRound(playerSelection, cpuSelection);
+
+        if (result === "win") playerScore++;
+        else if (result === "lose") cpuScore++;
+        roundNum++;
+    }
+
+    let result = (playerScore > cpuScore) ? "You Win!" : (cpuScore > playerScore) ? "You Lose :(" : "Meh. It's a tie. Better play again.";
+
+    let gameOverMsg = "Final Results\nNumber of rounds: " + roundNum + "\nPlayer: " + playerScore + "\nCPU Score: " + cpuScore + "\n\n" + result;
+
+    alert(gameOverMsg);
+}
+
+game();
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log("Player: " + playerSelection);
+// console.log("Computer: " + computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
